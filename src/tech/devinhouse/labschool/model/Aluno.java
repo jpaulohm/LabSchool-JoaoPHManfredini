de6cca2c763cbdbe80d73b689e6717a165be2fc6
Joao P Manfredini <jpaulohm@gmail.com>
2022-10-12 18:33:04 -0300

    Criado construtor pessoas

--- src/tech/devinhouse/labschool/model/Aluno.java
+++ src/tech/devinhouse/labschool/model/Aluno.java
@@ -1,5 +1,7 @@
 package tech.devinhouse.labschool.model;
 
+import java.time.LocalDate;
+
 public class Aluno extends Pessoa{
     SituacaoDaMatricula situacaoMatricula;
     Float notaProcessoSeletivo;
@@ -11,5 +13,17 @@ public class Aluno extends Pessoa{
 
     public void setSituacaoMatricula(SituacaoDaMatricula situacaoMatricula) {
         this.situacaoMatricula = situacaoMatricula;
+    }
+
+    public Aluno(String nome, Long cpf, LocalDate dataDeNascimento, Long telefone, SituacaoDaMatricula situacaoMatricula, Float notaProcessoSeletivo, Integer totalAtendPedag) {
+        this.nome = nome;
+        this.cpf = cpf;
+        //this.codigo = codigo;
+        this.telefone = telefone;
+        this.dataDeNascimento = dataDeNascimento;
+
+        this.situacaoMatricula = situacaoMatricula;
+        this.notaProcessoSeletivo = notaProcessoSeletivo;
+        this.totalAtendPedag = totalAtendPedag;
     }
 }
--- src/tech/devinhouse/labschool/model/Pedagogo.java
+++ src/tech/devinhouse/labschool/model/Pedagogo.java
@@ -1,8 +1,17 @@
 package tech.devinhouse.labschool.model;
 
+import java.time.LocalDate;
+
 public class Pedagogo extends Pessoa{
-    FormacaoAcademica formacao;
-    ExperienciaDesenvolvimento experiencia;
-    EstadoAtividade estado;
     Integer totalAtendPedag;
+
+    public Pedagogo(String nome, Long cpf, LocalDate dataDeNascimento, Long telefone, Integer totalAtendPedag) {
+        this.nome = nome;
+        this.cpf = cpf;
+        //this.codigo = codigo;
+        this.telefone = telefone;
+        this.dataDeNascimento = dataDeNascimento;
+
+        this.totalAtendPedag = 0;
+    }
 }
